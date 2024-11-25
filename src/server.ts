abe411dcf7e101dc6b273afc0093ff4a0113aeeb
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2024-11-25 10:37:07 +0600

    express and mongoose basic project setup done

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import app from "./app";
+import config from "./app/config";
+import mongoose from "mongoose";
+
+async function main() {
+
+    try {
+            await mongoose.connect(config.database_url as string);
+        // now need to connect with mongoose
+        app.listen(config.port, () => {
+        console.log(`Example app listening on port ${config.port}`)
+        })
+    } catch (err) {
+        console.log(err)
+    }
+    //local host use korbo na amra mongodb atlas use korbo
+}
+main()
+
